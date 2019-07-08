034f64e6af82619772e6b7a246028b20818e5366
Cooper Barth <cooperfbarth@gmail.com>
2019-07-08 13:52:27 -0500

    fixups

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use((req, res, next) => {
 	if (token) {
 		jwt.verify(token, process.env.JWT_SECRET, err => {
 			if (err) {
-				res.send(false, err, {});
+				res.send(response(false, err, {}));
 			} else {
 				res.header("Access-Control-Allow-Origin", "*");
 				res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
