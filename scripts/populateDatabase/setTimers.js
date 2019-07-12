fb98b43c8295bf1ed43b4a976f5ec3b270eb61cf
Cooper Barth <cooperfbarth@gmail.com>
2019-07-12 12:38:47 -0500

    re-added populate_subjects on a MAX_INT timer

--- scripts/populateDatabase/setTimers.js
+++ scripts/populateDatabase/setTimers.js
@@ -4,9 +4,9 @@ const env = "" + process.env.NODE_ENV;
 const config = require("../../config/db")[env || "development"];
 const logger = require("../../src/logger");
 
-const PING_TIMER = 1750000; //~29 mins
-// const SUBJECT_TIMER = 123456789; //DO NOT USE! -> this number must fit in a 32 bit signed int or it will default to 1
-const COURSE_TIMER = 604800000; //1 week
+const PING_TIMER = 1750000; // ~29 mins
+const SUBJECT_TIMER = Math.pow(2, 31) - 1; // max 32-bit int, ~24 days
+const COURSE_TIMER = 604800000; // 1 week
 
 function ping() {
     const axios = require("axios");
@@ -15,12 +15,11 @@ function ping() {
     }, PING_TIMER);
 }
 
-/* function populateSubjects() {
-    logger.warn("Do not set this function (populateSubjects) on a timer.");
+function populateSubjects() {
     setInterval(() => {
         require("./populateSubjects")();
     }, SUBJECT_TIMER);
-} */
+}
 
 function populateCourses() {
     setInterval(() => {
@@ -31,6 +30,6 @@ function populateCourses() {
 module.exports = () => {
     logger.info("Running database population scripts.");
     ping();
-    // populateSubjects();
+    populateSubjects();
     populateCourses();
 };
