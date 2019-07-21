b88a64ac6718eb75d469d4b014b9e3902b19053f
Cooper Barth <cooperfbarth@gmail.com>
2019-07-21 00:04:55 -0500

    Modified user routes

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -13,48 +13,54 @@ const User = require("../models/User");
 router.route("/createAccount")
     .post((req, res) => {
         validate(req.body, "creds", validResponse => {
+            const body = validResponse.body.data;
             if (validResponse.ok) {
-                User.findOne({email: validResponse.body.data.email}, (err, user) => {
+                User.findOne({email: body.email}, (err, user) => {
                     if (err) {
                         res.send(response(false, err, {_id: null}));
                     } else if (user) {
                         res.send(response(false, "User already exists."));
                     } else {
-                        const newUser = User.create(validResponse.body.data);
+                        const newUser = User.create(body);
                         newUser.save((err, user) => {
                             res.send(err? response(false, err) : response(true, "", {_id: user._id}));
                         });
                     }
                 });
             } else {
-                res.send(response(false, validResponse.message));
+                res.send(response(false, validResponse.message[0].message));
             }
         });
     });
 
 router.route("/login")
     .post((req, res) => {
-        const email = req.body.email;
-
-        User.findOne({email: email}, (err, user) => {
-            if (err) {
-                res.send(response(false, err));
-            } else if (!user) {
-                res.send(response(false, `No user with email ${email} found.`));
+        validate(req.body, "creds", validResponse => {
+            if (validResponse.ok) {
+                const body = validResponse.body.data;
+                User.findOne({email: body.email}, (err, user) => {
+                    if (err) {
+                        res.send(response(false, err));
+                    } else if (!user) {
+                        res.send(response(false, `No user with email ${body.email} found.`));
+                    } else {
+                        if (user.validateUser(body.password)) {
+                            const token = jwt({
+                                id: user._id,
+                                issued: Date.now()
+                            }, 10080);
+                            const cookieOptions = process.env.NODE_ENV === "production"? {httpOnly: true, secure: true} : {};
+                            res.cookie("access-token", token, cookieOptions);
+                            res.send(response(true, `User logged in.`, {id: user._id}));
+                        } else {
+                            res.send(response(false, "Failed to validate user."));
+                        }
+                    }
+                });
             } else {
-                if (user.validateUser(req.body.password)) {
-                    const token = jwt({
-                        id: user._id,
-                        issued: Date.now()
-                    }, 10080);
-                    const cookieOptions = process.env.NODE_ENV === "production"? {httpOnly: true, secure: true} : {};
-                    res.cookie("access-token", token, cookieOptions);
-                    res.send(response(true, `User logged in.`, {id: user._id}));
-                } else {
-                    res.send(response(false, "Failed to validate user."));
-                }
+                res.send(response(false, validResponse.message[0].message));
             }
-        });
+        })
     });
 
 router.route("/:id")
