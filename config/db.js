9032cda48555865c3424e7026a6c273bcca883f8
Cooper Barth <cooperfbarth@gmail.com>
2019-08-04 10:27:35 -0500

    Changed routes to proxy server

--- config/db.js
+++ config/db.js
@@ -7,8 +7,8 @@ module.exports = {
         "database":`mongodb+srv://dev:${process.env.MONGO_PW_DEV}@apollo-oq2zz.mongodb.net/test?retryWrites=true&w=majority`
     },
     "production":{
-        "host":"https://apollo-nu-server.herokuapp.com",
-        "clientHost":"https://apollo-nu.herokuapp.com",
+        "host":"https://apollo-nu-proxy.herokuapp.com/api",
+        "clientHost":"https://apollo-nu-proxy.herokuapp.com",
         "database":`mongodb+srv://prod:${process.env.MONGO_PW_PROD}@apollo-oq2zz.mongodb.net/prod?retryWrites=true&w=majority`
     }
 };
